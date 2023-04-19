6abd0d6f7d914514843745560ab7bef97c15836d
TTranchese <tommaso.tranchese.96@gmail.com>
2023-04-19 21:53:27 +0200

    intial commit

--- src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
+++ src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
@@ -1,15 +1,15 @@
 package co.develhope.firstapi4.controllers;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.http.HttpHeaders;
 
 @RestController
 public class HeaderController {
 	@GetMapping("/headers")
-	public String returnHeaders(@RequestHeader(HttpHeaders.REFERES)){
-
+	public String returnHeaders(@RequestHeader(HttpHeaders.HOST) String header) {
+		return header;
 	}
 }
